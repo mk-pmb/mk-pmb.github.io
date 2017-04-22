bc35c9d35d59a6d3a9b0f37b9c7f996b651c5048
mk-pmb <mk-pmb@users.noreply.github.com>
2017-04-22 15:05:40 +0200

    stub

---
+++ app/init.js
@@ -0,0 +1,18 @@
+﻿/*jslint indent: 2, maxlen: 80, continue: false, unparam: false */
+/* -*- tab-width: 2 -*- */
+/*global define: true, module: true, require: true */
+define(function (require) {
+  'use strict';
+
+  var EX, jq = require('jquery');
+
+  EX = function () {
+    jq('#app-init h2').first().text('stub.');
+    jq('#app-init p').first().text("There's no website yet.");
+  };
+
+
+
+
+  return EX;
+});
